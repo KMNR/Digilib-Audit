ca4fe0d69a9ff268184ffd4cc6264aaf5be7260c
Doug McGeehan <djmvfb@mst.edu>
2017-10-15 15:06:42 -0500

    Handling weird track numbers now.

--- library.py
+++ library.py
@@ -10,7 +10,7 @@ database_filename = 'music_library.db'
 
 def load_songs_from_directory(directory):
     songs = []
-    for directory, subdirectories, files in os.walk(dir):
+    for directory, subdirectories, files in os.walk(directory):
         for file in files:
             if file.lower().endswith(valid_extensions):
                 path = os.path.join(directory, file)
@@ -18,24 +18,28 @@ def load_songs_from_directory(directory):
                 try:
                     song = models.SongFile(file_path=path)
 
-                except ValueError:
+                except ValueError as e:
                     open('problems.txt', 'a').write('{}\n'.format(path))
-                    print("!! Problem parsing file '{}'".format(path))
+                    print(e)
                     continue
 
                 else:
                     print(song)
                     songs.append(song)
+
     return songs
 
 
-def build_db(dir):
-    if not os.path.isdir(dir):
-        raise IOError('"{}" is not a directory'.format(dir))
+def build_db(directory):
+    if not os.path.isdir(directory):
+        raise IOError('"{}" is not a directory'.format(directory))
 
     db.create_db(db_file_path=database_filename)
 
-    songs = load_songs_from_directory(directory=dir)
+    songs = load_songs_from_directory(directory=directory)
+
+    for k in sorted(models.SongFile.unique_keys_encountered):
+        print(k)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -3,25 +3,62 @@ import os
 
 
 class SongFile(object):
+    unique_keys_encountered = set()
+    attrs = ['tracknumber', 'title', 'artist', 'album', 'date']
+    converters = {
+        'tracknumber': int,
+        'date': int,
+    }
+
     def __init__(self, file_path):
         self.path = file_path
         self.filename = os.path.basename(file_path)
 
         # Extract the song's metadata from the music file
         metadata = mutagen.File(file_path, easy=True)
-        print(metadata.keys())
-        self.track_number = int(metadata.get('tracknumber', [None])[0])
         self.title = metadata.get('title', [None])[0]
         self.artist = metadata.get('artist', [None])[0]
         self.album = metadata.get('album', [None])[0]
-        self.year = int(metadata.get('date', [None])[0])
+        self.date = int(metadata.get('date', [None])[0])
+
+        # Track number could be a simple number, or XX/NN, where XX is the
+        # track number and NN is the total number of tracks.
+        tracknumber_string = metadata.get('tracknumber', [None])[0]
+        if tracknumber_string:
+            if '/' in tracknumber_string:
+                trackno, trackcount = tracknumber_string.split('/')
+                self.tracknumber = int(trackno)
+            else:
+                self.tracknumber = int(tracknumber_string)
+
+        # for key in self.attrs:
+        #     try:
+        #         raw_value = metadata.get(key, [None])[0]
+        #
+        #         if key in SongFile.converters:
+        #             value = SongFile.converters[key](raw_value)
+        #         else:
+        #             value = raw_value
+        #
+        #         setattr(self, key, value)
+        #     except ValueError:
+        #         raise ValueError('Problem with extracting {key}'
+        #                          ' from "{path}": value: {value}'.format(
+        #             key=key,
+        #             value=raw_value,
+        #             path=file_path
+        #         ))
+
+        self.unique_keys_encountered.add(
+            ','.join(sorted(metadata.keys()))
+        )
 
     def __str__(self):
         return '"{track_number}. {title}" by {artist}' \
                ' off of the album "{album}" ({year})'.format(
-            track_number=self.track_number,
+            track_number=self.tracknumber,
             title=self.title,
             artist=self.artist,
             album=self.album,
-            year=self.year,
+            year=self.date,
         )
