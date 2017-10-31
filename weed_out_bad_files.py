00b3152fe77eb192eeb0aa1d8ed9034f42838cd7
Doug McGeehan <djmvfb@mst.edu>
2017-10-31 07:20:56 -0500

    create a list of bad files for removal

--- weed_out_bad_files.py
+++ weed_out_bad_files.py
@@ -11,7 +11,7 @@ audio_file_extensions = ('.wav', '.flac', '.mp3', '.mp4', '.wma', '.m4a', '.dts'
 encountered_extensions = collections.defaultdict(int)
 
 
-with open('problem_files.txt', 'w') as f:
+with open('weed_out_files.txt', 'w') as f:
     for d, subdir, files in os.walk(directory):
         if d.endswith('__MACOSX'):
             shutil.rmtree(d)
